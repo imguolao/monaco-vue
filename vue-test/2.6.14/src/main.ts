a21a251a4bfd4f309de55af7613bff70d1651dae
imguolao <guolao.mq@foxmail.com>
2023-02-22 10:42:53 +0800

    fix: add container vnode key

--- vue-test/2.6.14/src/main.ts
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import VueCompositionAPI, { createApp, h } from '@vue/composition-api'
-import App from './App'
-
-Vue.use(VueCompositionAPI)
-
-createApp({ render: () => h(App) }).mount('#app')
