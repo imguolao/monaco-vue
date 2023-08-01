4de436f7f349a08132568738df2a28f4bf41c1fd
Cat1007 <54425790+Cat1007@users.noreply.github.com>
2023-08-02 00:05:04 +0800

    features: export monaco types (#18)
    Co-authored-by: catye <catye@tencent.com>

--- packages/editor/src/index.ts
+++ packages/editor/src/index.ts
@@ -2,6 +2,7 @@ import loader from '@monaco-editor/loader'
 import VueMonacoEditor from './components/Editor'
 
 export type { EditorProps, VueMonacoEditorEmitsOptions } from './components/Editor'
+export type { MonacoEditor } from './types/index';
 
 export { install } from './install'
 export { useMonaco } from './hooks'
