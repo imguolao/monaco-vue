37fe992605644cb311a922f7564743414cec6537
JeanJPNM <jeanjpnm1@gmail.com>
2023-09-11 00:43:26 -0300

    initialize the monaco loader after mounting
    This prevents the loader from running during SSR builds

--- packages/editor/src/hooks/useMonaco.ts
+++ packages/editor/src/hooks/useMonaco.ts
@@ -1,21 +1,28 @@
 import type { Nullable, MonacoEditor } from '../types'
-import { shallowRef } from 'vue-demi'
+import { onMounted, shallowRef } from 'vue-demi'
 import loader from '@monaco-editor/loader'
 
 export function useMonaco() {
-  const monacoRef = shallowRef<Nullable<MonacoEditor>>(null)
+  const monacoRef = shallowRef<Nullable<MonacoEditor>>(loader.__getMonacoInstance())
+
+  let promise: ReturnType<(typeof loader)['init']>
+
+  onMounted(() => {
+    // the instance has already been loaded
+    if (monacoRef.value) return
+
+    promise = loader.init()
+    promise
+      .then(monacoInstance => (monacoRef.value = monacoInstance))
+      .catch(error => {
+        if (error?.type !== 'cancelation') {
+          console.error('Monaco initialization error:', error)
+        }
+      })
+  })
 
   // monaco mount
-  const monacoLoader = loader.init()
-  const unload = () => monacoLoader.cancel()
-
-  monacoLoader
-    .then(monacoInstance => (monacoRef.value = monacoInstance))
-    .catch(error => {
-      if (error?.type !== 'cancelation') {
-        console.error('Monaco initialization error:', error)
-      }
-    })
+  const unload = () => promise?.cancel()
 
   return {
     monacoRef,
