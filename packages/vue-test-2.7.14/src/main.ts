c4d28fd128931994d5dad46d8a70da03f6aff794
imguolao <guolao.mq@foxmail.com>
2025-03-15 17:02:04 +0800

    refactor: update loader to 1.5.0 version

--- packages/vue-test-2.7.14/src/main.ts
+++ packages/vue-test-2.7.14/src/main.ts
@@ -4,7 +4,7 @@ import App from './App.vue'
 
 loader.config({
   paths: {
-    vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.43.0/min/vs',
+    vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.52.2/min/vs',
   },
 })
 
