e2ba6a82b1ba6f2bd27a04dc2ffa4c3a09938cbe
imguolao <guolao.mq@foxmail.com>
2025-10-20 02:45:41 +0800

    feat: update loader to v1.6.1

--- packages/vue-test-2.7.14/src/main.ts
+++ packages/vue-test-2.7.14/src/main.ts
@@ -4,7 +4,7 @@ import App from './App.vue'
 
 loader.config({
   paths: {
-    vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.52.2/min/vs',
+    vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.54.0/min/vs',
   },
 })
 
